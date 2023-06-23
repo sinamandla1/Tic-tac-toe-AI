78080301f8c017ccb3071ef5e721da297c3eac6e
sinamandla1 <sinamandlableu@gmail.com>
2023-06-23 15:31:14 +0200

    new file

---
+++ player.py
@@ -0,0 +1,25 @@
+import math
+import random
+
+
+class Player:
+    def __init__(self,letter):
+        self.letter = letter
+
+    def get_move(self, game):
+        pass
+
+class RandomCpuPlayer(Player):
+    def __int__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        pass
+
+class HumanPlayer(Player):
+    def __int__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        pass
+
