ad5981f43544df2f0529df6041bd70707de6b883
mat-t-hew <n.m.ikenna2@gmail.com>
2023-06-30 01:20:37 +0200

    updatedcode

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ from player import HumanPlayer, RandomCpuPlayer
 
 class TicTacToe():
     def __init__(self):
-        self.board =[' ' for _ in range(9)]
+        self.board = [' ' for _ in range(9)]
         self.current_winner = None
 
     def print_board(self):
         for row in [self.board[i*3:(i+1)*3] for i in range(3)]:
             print('| ' + ' | '.join(row) + ' |')
-  
+
     @staticmethod
     def print_board_nums():
         # 0 | 1 | 2
@@ -17,18 +17,18 @@ class TicTacToe():
             print('| ' + ' | '.join(row) + ' |')
 
     def available_moves(self):
-        moves =[]
-        for (i,spot) in enumerate(self.board):
+        moves = []
+        for (i, spot) in enumerate(self.board):
             if spot == ' ':
                 moves.append(i)
-                return moves
-            
+        return moves
+
     def empty_squares(self):
         return ' ' in self.board
-    
+
     def num_empty_squares(self):
         return self.board.count(' ')
-    
+
     def make_move(self, square, letter):
         if self.board[square] == ' ':
             self.board[square] = letter
@@ -36,18 +36,18 @@ class TicTacToe():
                 self.current_winner = letter
             return True
         return False
-    
+
     def winner(self, square, letter):
         row_ind = square // 3
-        row = self.board[row_ind+3 : (row_ind + 1) * 3]
+        row = self.board[row_ind*3: (row_ind + 1) * 3]
         if all([spot == letter for spot in row]):
             return True
-        
+
         col_ind = square % 3
-        column = [self.board[col_ind+i*3] for i in range(3)]
+        column = [self.board[col_ind + i*3] for i in range(3)]
         if all([spot == letter for spot in column]):
             return True
-        
+
         if square % 2 == 0:
             diagonal1 = [self.board[i] for i in [0, 4, 8]]
             if all([spot == letter for spot in diagonal1]):
@@ -56,7 +56,8 @@ class TicTacToe():
             if all([spot == letter for spot in diagonal2]):
                 return True
         return False
-    
+
+
 def play(game, x_player, o_player, print_game=True):
     if print_game:
         game.print_board_nums()
@@ -66,31 +67,33 @@ def play(game, x_player, o_player, print_game=True):
     while game.empty_squares():
         if letter == 'O':
             square = o_player.get_move(game)
-    else:
-        square = x_player.get_move(game)
-    
-    if game.make_move(square, letter):
-        if print_game:
-            print(letter + f' makes a move to square {square}')
-            game.print_board()
-            print('')
-
-        if game.current_winner:
-            if print_game:
-                print(letter + ' wins!')
-            return letter
-
-        if letter == 'X':
-            letter = 'O'
         else:
-            letter = 'X'
-    if print_game:
-        print('It\'s a tie!')        
+            square = x_player.get_move(game)
 
-        if __name__ == '__main__':
-             x_player = HumanPlayer('X')
-             o_player = RandomCpuPlayer('O')
-             t = TicTacToe()
-             play(t, x_player, o_player, print_game=True)
-    
-    
+        if game.make_move(square, letter):
+            if print_game:
+                print(letter + f' makes a move to square {square}')
+                game.print_board()
+                print('')
+
+            if game.current_winner:
+                if print_game:
+                    print(letter + ' wins!')
+                return letter
+
+            if letter == 'X':
+                letter = 'O'
+            else:
+                letter = 'X'
+        else:
+            print('Invalid move. Try again.')
+            
+    if print_game:
+        print('It\'s a tie!')
+
+
+if __name__ == '__main__':
+    x_player = HumanPlayer('X')
+    o_player = RandomCpuPlayer('O')
+    t = TicTacToe()
+    play(t, x_player, o_player, print_game=True)
--- player.py
+++ player.py
@@ -1,24 +1,22 @@
-import math
 import random
 
-
 class Player:
-    def __init__(self,letter):
+    def __init__(self, letter):
         self.letter = letter
 
     def get_move(self, game):
         pass
 
 class RandomCpuPlayer(Player):
-    def __int__(self, letter):
+    def __init__(self, letter):
         super().__init__(letter)
 
     def get_move(self, game):
-        square = random.choice(game.availablemoves())
+        square = random.choice(game.available_moves())
         return square
 
 class HumanPlayer(Player):
-    def __int__(self, letter):
+    def __init__(self, letter):
         super().__init__(letter)
 
     def get_move(self, game):
@@ -26,12 +24,13 @@ class HumanPlayer(Player):
         val = None
         
         while not valid_square:
-            square = input(self.letter + '\'s turn Input move (0 - 9): ')
+            square = input(self.letter + "'s turn. Enter a move (0-8): ")
             try:
                 val = int(square)
-                if val not in game.availablemoves():
+                if val not in game.available_moves():
                     raise ValueError
                 valid_square = True
             except ValueError:
-                print("Invalid square, try again!")
-                return val
+                print("Invalid move. Try again!")
+
+        return val
