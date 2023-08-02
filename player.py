5fabb6ded7ff0a521f0afa180c54bf7f771bdf73
Sinamandla <121508711+sinamandla1@users.noreply.github.com>
2023-08-02 15:54:59 +0200

    Update player.py
    added AI player

--- player.py
+++ player.py
@@ -7,9 +7,38 @@ class Player:
     def get_move(self, game):
         pass
 
-class RandomCpuPlayer(Player):
-    def __init__(self, letter):
-        super().__init__(letter)
+class Ai_player(Player):
+  def __init__(self, letter):
+    super().__init__(letter)
+  
+  def get_move(self, game):
+      for i in game.available_moves():
+        boardcopy = game.board[:]
+        boardcopy[i] = self.letter
+        
+        if game.winner(i, self.letter):
+          return i
+
+      player_letter = 'X' if self.letter == 'O' else 'O'
+      for i in game.available_moves():
+        boardcopy = game.board[:]
+        boardcopy[i] = player_letter
+            
+        if game.winner(i, player_letter):
+          return i
+
+      if 4 in game.available_moves():
+        return 4
+
+      open_corners = [move for move in game.available_moves() if move in [0, 2, 6, 8]]
+      if open_corners:
+        return random.choice(open_corners)
+      
+      open_edges = [move for move in game.available_moves() if move in [1, 3, 5, 7]]
+      if open_edges:
+        return random.choice(open_edges)
+      
+      return random.choice(game.available_moves())
 
     def get_move(self, game):
         square = random.choice(game.available_moves())
